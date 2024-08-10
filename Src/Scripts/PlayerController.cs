9092d8c8eed3bbf3847ae7adf4e9f63b36c38392
OverLuked <edillorluke@gmail.com>
2024-08-10 16:54:23 +0800

    ADD: Player Animations

--- Src/Scripts/PlayerController.cs
+++ Src/Scripts/PlayerController.cs
@@ -14,10 +14,13 @@ public partial class PlayerController : Node
 	private Vector2 _direction;
 	private Vector2 _velocity;
 	private CharacterBody2D _player;
+	private AnimatedSprite2D _anims;
+	private double _angle;
+	
 
-	public void setStats(float health, float speed, float attackSpeed, float bulletSpeed, float damage, int n, int maxDash, CharacterBody2D player)
+	public void SetStats(float health, float speed, float attackSpeed, float bulletSpeed, float damage, 
+		int n, int maxDash, CharacterBody2D player, AnimatedSprite2D anims)
 	{
-		_health = health;
 		_speed = speed;
 		_attackSpeed = attackSpeed;
 		_bulletSpeed = bulletSpeed;
@@ -25,11 +28,34 @@ public partial class PlayerController : Node
 		_dashN = n;
 		_maxDash = maxDash;
 		_player = player;
+		_anims = anims;
+	}
+	private void _Animations()
+	{
+		_angle = _player.GetGlobalMousePosition().AngleToPoint(_player.Position);
+		
+		switch (_angle)
+		{
+			case > -0.8 and < 0.6:
+				_anims.Play("WalkLeft");
+				break;
+			case >= 0.6 and < 2.6:
+				_anims.Play("WalkUp");
+				break;
+			case >= 2.6 and <= 3.14:
+			case <= -2.14 and >= -3.14:
+				_anims.Play("WalkRight");
+				break;
+			default:
+				_anims.Play("WalkDown");
+				break;
+		}
 	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Player Controller is Ready!");
+		GD.Print("Player Animations Ready");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,6 +70,7 @@ public partial class PlayerController : Node
 		_player.Velocity = _velocity != Vector2.Zero
 			? _player.Velocity.Lerp(_velocity * _speed * 2, 0.35f)
 			: _player.Velocity.Lerp(Vector2.Zero, 0.5f);
+		_Animations();
 
 		_player.MoveAndSlide();
 	}
--- Src/Scripts/alexander_slime.cs
+++ Src/Scripts/alexander_slime.cs
@@ -4,6 +4,7 @@ using System;
 public partial class alexander_slime : CharacterBody2D
 {
     [Export] private PlayerController _controller;
+    [Export] private AnimatedSprite2D _anims;
     
     [Export] private float _health = 3.5f;
     [Export] private float _speed = 25.0f;
@@ -13,10 +14,17 @@ public partial class alexander_slime : CharacterBody2D
     [Export] private int _dashN = 1;
     [Export] private int _maxDash = 3;
 
+    private double _direction;
+
     
     public override void _Ready()
     {
         GD.Print("PLAYER READY");
-        _controller.setStats(_health, _speed, _attackSpeed, _bulletSpeed, _damage, _dashN, _maxDash, this);
+        _controller.SetStats(_health, _speed, _attackSpeed, _bulletSpeed, _damage, _dashN, _maxDash, this, _anims);
+    }
+
+    public override void _Process(double delta)
+    {
+        _direction = GetGlobalMousePosition().AngleToPoint(this.Position);
     }
 }
